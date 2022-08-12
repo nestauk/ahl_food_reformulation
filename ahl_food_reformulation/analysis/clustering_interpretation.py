1c41a6104c9b8e8a96119d1e46efacc9f6a7619e
Juan Mateos-Garcia <juan.mateos-garcia@nesta.org.uk>
2022-08-12 14:33:11 +0100

    cluster interpretation and analysis

--- ahl_food_reformulation/analysis/clustering_interpretation.py
+++ ahl_food_reformulation/analysis/clustering_interpretation.py
@@ -1,16 +1,22 @@
 # Functions to explore the cluster data
 
 import re
+from functools import partial
+import random
+from toolz import pipe
 from typing import Dict, Any, List, Union, Tuple
 
 import altair as alt
 import numpy as np
 import pandas as pd
+from scipy.stats import zscore
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
+
+import statsmodels.api as sm
 
 from ahl_food_reformulation import PROJECT_DIR
 from ahl_food_reformulation.getters import kantar
@@ -321,6 +327,289 @@ def plot_regression_coeffs(reg_coeff: pd.DataFrame) -> alt.Chart:
                 scale=alt.Scale(scheme="redblue"),
                 sort="descending",
                 title=["Regression", "coefficient"],
+            ),
+        )
+    )
+
+
+def plot_demog_pipeline(
+    demog: pd.DataFrame,
+    comp_variable: str,
+    order_vars: List,
+    drops: List = ["Unknown", "Did not want to answer", "Other"],
+) -> alt.Chart:
+    """Pipeline to generate demographic plots
+    Args:
+        demog: household characteristics table
+        comp_variable: variable to compare across clusters
+        order_vars: order of categories for the variable
+        drops: categories to drop
+
+    Returns:
+        A plot comparing clusters along a demographic variable
+
+    """
+    return pipe(
+        demog,
+        partial(calculate_cluster_shares, variable=comp_variable),
+        partial(
+            plot_cluster_comparison_cat,
+            drop=drops,
+            pos_text=3,
+            var_order=order_vars,
+            var_name=comp_variable,
+        ),
+    )
+
+
+def make_salience_table(
+    coefficients_df: pd.DataFrame,
+    coeff_name: str = "coefficient",
+    comp_var: str = "variable",
+    top_n: int = 10,
+) -> pd.DataFrame:
+    """Create table with top regression scores
+
+    Args:
+        coefficients_df: dataframe with regression coefficients
+        coeff_name: name of the column with the coefficients
+        comp_var: name of the column with the variable
+        top_n: number of top coefficients to keep
+
+    Returns:
+        A dataframe with the top coefficients
+    """
+    return (
+        coefficients_df.groupby("cluster")
+        .apply(
+            lambda df: (
+                df.assign(
+                    high_predictive=lambda df: ", ".join(
+                        df.sort_values(coeff_name, ascending=False).head(n=top_n)[
+                            comp_var
+                        ]
+                    )
+                )
+                .assign(
+                    low_predictive=lambda df: ", ".join(
+                        df.sort_values(coeff_name, ascending=True).head(n=top_n)[
+                            comp_var
+                        ]
+                    )
+                )[["cluster", "high_predictive", "low_predictive"]]
+                .drop_duplicates("cluster")
+            )
+        )
+        .reset_index(drop=True)
+    )
+
+
+def item_share(purchase_table: pd.DataFrame, category: str) -> pd.Series:
+    """Share of items in a category"""
+
+    return (
+        purchase_table.groupby(category).apply(lambda df: df["volume"].sum())
+        / purchase_table["volume"].sum()
+    )
+
+
+def plot_item_share(distribution: pd.Series, category: str) -> alt.Chart:
+    """Plot share of items in a category"""
+
+    return (
+        alt.Chart(distribution.reset_index(name="share"))
+        .mark_line(point=True, stroke="red")
+        .encode(
+            y=alt.Y(category, sort=alt.EncodingSortField("share", order="descending")),
+            x=alt.X("share", title="Share of volume", axis=alt.Axis(format="%")),
+        )
+    )
+
+
+def make_purchase_shares_normalised(
+    purchase_records: pd.DataFrame, category: str, top_n: int = 5, num_clust: int = 18
+) -> pd.DataFrame:
+    """Creates normalised purchase shares
+    Args:
+        purchase_records: dataframe with purchase records
+        category: category to group by
+        top_n: number of top categories to keep
+        num_clust: number of clusters
+
+    Returns:
+        Table with normalised purchase shares
+    """
+
+    purchase_shares_cluster = purchase_records.groupby(["clust"]).apply(
+        lambda df: (
+            df.groupby(category)["volume"].sum() / df["volume"].sum()
+        ).sort_values(ascending=False)
+    )
+
+    return (
+        purchase_shares_cluster.unstack(level=1)
+        .fillna(0)
+        .apply(lambda col: zscore(col))
+        .stack()
+        .reset_index(name="share_normalised")
+        .groupby(category)  # This is to extract labels for outlierish clusters
+        .apply(
+            lambda df: df.assign(
+                add_label=[
+                    text if (rank < top_n) | (rank > num_clust - top_n) else ""
+                    for text, rank in zip(df["clust"], df["share_normalised"].rank())
+                ]
+            )
+        )
+        .reset_index(drop=False)
+    )
+
+
+def plot_shares_normalised(
+    purchase_normalised: pd.DataFrame, category: str, width: int = 700
+) -> alt.Chart:
+    """Plots nornalised shares
+    Args:
+        purchase_normalised: dataframe with normalised purchase shares
+        category: name of the category variable
+        width: width of the plot
+
+    Returns:
+        Altair chart with normalised purchase shares
+    """
+
+    ch = (
+        alt.Chart(purchase_normalised)
+        .mark_point(filled=True, size=50, stroke="black", strokeWidth=0.5)
+        .encode(
+            y=category,
+            x=alt.X("share_normalised", title="Share of volume, normalised"),
+            tooltip=["clust"],
+            color=alt.Color("clust:O", scale=alt.Scale(scheme="tableau20")),
+        )
+    )
+
+    # This shows labels for the top categories
+    txt = (
+        alt.Chart(purchase_normalised)
+        .mark_text(dx=10, dy=-4)
+        .encode(
+            y=category,
+            x=alt.X("share_normalised", title="Share of volume, normalised"),
+            tooltip=["clust"],
+            text="add_label",
+            color=alt.Color("clust:O", scale=alt.Scale(scheme="tableau20")),
+        )
+    )
+
+    rul = (
+        alt.Chart(pd.DataFrame({"x": [1]}))
+        .mark_rule(strokeDash=[2, 2], stroke="red")
+        .encode(x="x")
+    )
+
+    return (ch + rul + txt).properties(width=width)
+
+
+def make_regression_dataset(
+    purchase_records: pd.DataFrame,
+    clust_lu: Dict,
+    demog_table: pd.DataFrame,
+    category: str,
+    sample_size: int = 5000,
+) -> Tuple[pd.DataFrame]:
+    """Creates a regression dataset
+
+    Args:
+        purchase_records: dataframe with purchase records
+        clust_lu: dictionary with cluster labels
+        demog_table: dataframe with demographic data
+        category: category to group by
+        sample_size: sample size to use
+
+    Returns:
+        A tuple with the targets (consumption) and the features (cluster + household size control)
+
+    """
+
+    house_sample = random.sample(
+        purchase_records.dropna(axis=0, subset=["clust"])["panel_id"].unique().tolist(),
+        sample_size,
+    )
+
+    purchase_records_sample = purchase_records.loc[
+        purchase_records["panel_id"].isin(house_sample)
+    ].reset_index(drop=True)
+
+    print(len(purchase_records_sample))
+
+    househ_shares_target = (
+        purchase_records_sample.groupby(["panel_id"])
+        .apply(lambda df: df.groupby(category)["volume"].sum() / df["volume"].sum())
+        .unstack(level=1)
+        .fillna(0)
+        .apply(lambda x: zscore(x))
+    )
+
+    househ_features = (
+        househ_shares_target.reset_index(drop=False)["panel_id"]
+        .to_frame()
+        .assign(cluster=lambda df: df["panel_id"].map(clust_lu))
+        .assign(
+            hous_size=lambda df: df["panel_id"].map(
+                demog_table.set_index("panel_id")["household_size"].to_dict()
+            )
+        )
+    )
+
+    return househ_shares_target, househ_features
+
+
+def fit_purchase_regression(
+    target: pd.DataFrame, features: pd.DataFrame, category
+) -> pd.DataFrame:
+    """Regresses share of purchases on household basket on cluster + household size"""
+
+    coeffs = []
+    print("Regressing...")
+    for cat in target.columns:
+        print("\t" + cat)
+
+        for clust in features["cluster"].unique():
+
+            endog = target[cat].to_numpy()
+            exog = sm.add_constant(
+                features.drop(axis=1, labels=["panel_id"]).assign(
+                    cluster=lambda df: (df["cluster"] == clust).astype(int)
+                )
+            ).to_numpy()
+
+            mod = sm.OLS(endog, exog)
+            out = mod.fit()
+            coeffs.append([cat, clust, out.params[1], out.pvalues[1]])
+
+    return pd.DataFrame(coeffs, columns=[category, "cluster", "coefficient", "p_value"])
+
+
+def plot_regression_result(
+    regression_table: pd.DataFrame,
+    pvalue_thres: float = 0.05,
+    category: str = "rst_4_market_sector",
+) -> alt.Chart:
+    """Plots regression results"""
+
+    return (
+        alt.Chart(regression_table.query(f"p_value<{pvalue_thres}"))
+        .mark_rect(stroke="darkgrey", strokeWidth=0.5)
+        .encode(
+            y=category,
+            x="cluster:O",
+            tooltip=[category, "cluster", alt.Tooltip("coefficient", format=".3f")],
+            color=alt.Color(
+                "coefficient",
+                scale=alt.Scale(scheme="redblue", domainMid=0),
+                sort="descending",
+                title="Regression coefficient",
             ),
         )
     )
