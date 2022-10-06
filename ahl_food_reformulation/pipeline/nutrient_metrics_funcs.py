d37070a3d1dc2d2a7c4ebae9618a6985e46e4cbf
Izzy Stewart <isabelrstewart@gmail.com>
2022-10-06 15:30:43 +0100

    adding functions and script for nutrient analysis (#53)
    * adding functions and script for nutrient analysis
    
    * fixing a typo and updating entropy function to include deciles
    
    Co-authored-by: Isabel <isabel.stewart@nesta.org.uk>

---
+++ ahl_food_reformulation/pipeline/nutrient_metrics_funcs.py
@@ -0,0 +1,147 @@
+# Import libraries
+from ahl_food_reformulation import PROJECT_DIR
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
+from scipy.stats import entropy
+from sklearn.preprocessing import MinMaxScaler
+
+
+def rank_col(df_col):
+    return df_col.rank(ascending=False).astype(int)
+
+
+def cat_entropy(df, cat):
+    """
+    Calculates the entropy value per category of products
+    Args:
+        df (pd.DataFrame): Pandas dataframe kcal per 100g/ml per product
+        cat (str): Product category
+    Returns:
+        pd.Series: Series with entropy per category
+    """
+    df["deciles"] = pd.qcut(df["kcal_100g_ml"], 10, labels=False)
+    return df.groupby(cat)["deciles"].apply(lambda x: entropy(x.value_counts(), base=2))
+
+
+def cat_variance(df, cat):
+    """
+    Calculates the variance per category of products
+    Args:
+        df (pd.DataFrame): Pandas dataframe kcal per 100g/ml per product
+        cat (str): Product category
+    Returns:
+        pd.Series: Series with variance per category
+    """
+    return df.groupby(cat)["kcal_100g_ml"].var()
+
+
+def avg_metric_samples(df, num_runs, cat, size):
+    """
+    Calculates average entropy and variance per category of products per selected sample size and number of runs
+    Args:
+        df (pd.DataFrame): Pandas dataframe kcal per 100g/ml per product
+        num_runs (int): Number of times to sample the data and collect results
+        cat (str): Product category
+        size (int): Size of sample
+    Returns:
+        pd.DataFrame: Dataframe of avg entropy and variance per category of products
+    """
+    ent_list = []
+    var_list = []
+    for i in range(num_runs):
+        sample = df.groupby(cat).sample(n=size)
+        ent_list.append(cat_entropy(sample, cat).values)
+        var_list.append(cat_variance(sample, cat).values)
+    return pd.DataFrame(
+        {
+            "entropy_size_adj": np.mean(ent_list, axis=0),
+            "variance_size_adj": np.mean(var_list, axis=0),
+            cat: cat_entropy(sample, cat).index,
+        }
+    )
+
+
+def create_diversity_df(df, cat, n_runs, sample_size):
+    """
+    Applies the entropy and variance functions to get total results for df and avg per samples
+    Args:
+        df (pd.DataFrame): Pandas dataframe kcal per 100g/ml per product
+        num_runs (int): Number of times to sample the data and collect results
+        cat (str): Product category
+        sample_size (int): Size of sample
+    Returns:
+        pd.DataFrame: Dataframe of total and avg per samples entropy and variance per category of products
+    """
+    scaler = MinMaxScaler()  # Add scaler (minmax)
+    df_diversity = pd.concat(
+        [df.groupby(cat).size(), cat_entropy(df, cat), cat_variance(df, cat)], axis=1
+    )
+    df_diversity.columns = ["count", "entropy", "variance"]
+    avg_metrics = avg_metric_samples(df, n_runs, cat, sample_size)
+    df_merged = df_diversity.merge(avg_metrics, on=cat)
+    df_merged[["variance_scaled", "variance_adj_scaled"]] = scaler.fit_transform(
+        df_merged[["variance", "variance_size_adj"]]
+    )
+    df_merged[["var_rank", "var_adj_rank"]] = df_merged[
+        ["variance", "variance_size_adj"]
+    ].apply(rank_col)
+    return df_merged
+
+
+def diversity_heatmaps(df_diversity, cat, entropy_values, variance_values, filename):
+    """
+    Plots heatmaps of count, entropy and variance metrics and saves file
+    Args:
+        df_diversity (pd.DataFrame): Pandas dataframe nutrient diversity metrics
+        cat (str): Product category
+        entropy_values (str): entropy column name
+        variance_values (str): variance column name
+        filename (str): String to append to filename
+    Returns:
+        Heatmap plots
+    """
+    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(15, 8))
+
+    cmap = sns.cm.rocket_r
+
+    sns.heatmap(
+        df_diversity.set_index(cat)[[entropy_values]].sort_values(
+            by=entropy_values, ascending=False
+        ),
+        ax=ax1,
+        cmap=cmap,
+    )
+    sns.heatmap(
+        df_diversity.set_index(cat).sort_values(by=entropy_values, ascending=False)[
+            [variance_values]
+        ],
+        ax=ax2,
+        cmap=cmap,
+    )
+    sns.heatmap(
+        df_diversity.set_index(cat).sort_values(by=entropy_values, ascending=False)[
+            ["count"]
+        ],
+        ax=ax3,
+        cmap=cmap,
+    )
+
+    for ax in (ax1, ax2, ax3):
+        ax.set_xlabel("Method")
+        ax.set_ylabel("Category")
+
+    plt.tight_layout()
+    # Add folder if not already created
+    Path(f"{PROJECT_DIR}/outputs/figures/nutrient_diversity/").mkdir(
+        parents=True, exist_ok=True
+    )
+    fig.savefig(
+        f"{PROJECT_DIR}/outputs/figures/nutrient_diversity/diversity_heatmaps_"
+        + filename
+        + ".png",
+        bbox_inches="tight",
+    )
+    plt.show(block=False)
