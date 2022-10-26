e2b96b40450e4d6fc4cf0f250350fafb84fbaf7b
ElenaMariani <elena.mariani@nesta.org.uk>
2022-10-26 09:42:55 +0100

    Removed outliers and fine tuned calculation of daily kcal

--- ahl_food_reformulation/analysis/impact/impact-kcal.py
+++ ahl_food_reformulation/analysis/impact/impact-kcal.py
@@ -3,7 +3,6 @@ from ahl_food_reformulation.getters import kantar
 import logging
 import json
 import pandas as pd
-from ahl_food_reformulation.pipeline import transform_data as transform
 from ahl_food_reformulation import PROJECT_DIR
 from ahl_food_reformulation.pipeline import kcal_impact as kcal
 
@@ -15,19 +14,15 @@ if __name__ == "__main__":
     pur_recs = kantar.purchase_records_updated()
     nut_rec = kantar.nutrition()
     pan_ind = kantar.household_ind()
-    prod_mast = kantar.product_master()
     val_fields = kantar.val_fields()
     uom = kantar.uom()
     prod_codes = kantar.product_codes()
     prod_vals = kantar.product_values()
     panel_weight = kantar.panel_weights_year()
-    cl_kcal_share = kantar.cluster_kcal_share()
-    cl_adj_size = kantar.cluster_adj_size()
     prod_meta = kantar.prod_meta_update()
     kcal_est = kantar.kcal_reduction()
 
     # define cats
-    att_num = 2907  # code corresponding to rst_4_extended
     broader_category = "rst_4_market_sector"
     granular_category = "rst_4_extended"
 
@@ -38,57 +33,46 @@ if __name__ == "__main__":
         )
 
     logging.info("Merging data")
-    # merge files to get calorie reduction for product categories
-    target_red = chosen_cats.merge(kcal_est, on="rst_4_market_sector")
 
-    # generate conversion factor
-    pan_conv = transform.hh_size_conv(pan_ind)
-
-    # Purchase and product info combined
-    comb_files = transform.combine_files(
-        val_fields, pur_recs, prod_codes, prod_vals, att_num
-    ).drop("att_vol", axis=1)
-
-    comb_update = comb_files.merge(
-        prod_meta[["product_code", "rst_4_extended", "rst_4_market_sector"]],
-        left_on="Product Code",
-        right_on="product_code",
+    purch_recs_comb_scenarios = kcal.make_impact(
+        chosen_cats,
+        kcal_est,
+        pan_ind,
+        val_fields,
+        pur_recs,
+        prod_codes,
+        prod_vals,
+        prod_meta,
+        nut_rec,
     )
-
-    comb_update.rename(columns={"rst_4_extended": "att_vol"}, inplace=True)
-
-    # sum of all kcal purchased by category by an household
-    purch_recs_comb = transform.make_purch_records(nut_rec, comb_update, ["att_vol"])
-
-    purch_recs_comb_scenarios = purch_recs_comb.merge(
-        target_red, right_on="rst_4_extended", left_on="att_vol", how="left"
-    ).fillna(0)
-
-    # remove outliers
-    # q_low = purch_recs_comb_scenarios["Gross_up_kcal"].quantile(0.05)
-    # q_hi  = purch_recs_comb_scenarios["Gross_up_kcal"].quantile(0.95)
-
-    # pur_rec_filter = purch_recs_comb_scenarios[(purch_recs_comb_scenarios["Gross_up_kcal"] < q_hi) & (purch_recs_comb_scenarios["Gross_up_kcal"] > q_low)].copy()
-
-    purch_recs_comb_scenarios["Gross_up_kcal_min"] = purch_recs_comb_scenarios[
-        "Gross_up_kcal"
-    ] * (1 - purch_recs_comb_scenarios["min"])
-    purch_recs_comb_scenarios["Gross_up_kcal_max"] = purch_recs_comb_scenarios[
-        "Gross_up_kcal"
-    ] * (1 - purch_recs_comb_scenarios["max"])
 
     logging.info("Generating Descriptive Stats")
 
     pd.concat(
         [
             kcal.kcal_day(
-                purch_recs_comb_scenarios, pan_conv, panel_weight, "Gross_up_kcal"
+                purch_recs_comb_scenarios,
+                pan_ind,
+                panel_weight,
+                "Gross_up_kcal",
+                0.05,
+                0.95,
             ),
             kcal.kcal_day(
-                purch_recs_comb_scenarios, pan_conv, panel_weight, "Gross_up_kcal_min"
+                purch_recs_comb_scenarios,
+                pan_ind,
+                panel_weight,
+                "Gross_up_kcal_min",
+                0.05,
+                0.95,
             ),
             kcal.kcal_day(
-                purch_recs_comb_scenarios, pan_conv, panel_weight, "Gross_up_kcal_max"
+                purch_recs_comb_scenarios,
+                pan_ind,
+                panel_weight,
+                "Gross_up_kcal_max",
+                0.05,
+                0.95,
             ),
         ],
         axis=1,
--- ahl_food_reformulation/pipeline/kcal_impact.py
+++ ahl_food_reformulation/pipeline/kcal_impact.py
@@ -1,4 +1,91 @@
-def kcal_day(purch_recs_comb, pan_conv, panel_weight, scenario):
+from ahl_food_reformulation.pipeline import transform_data as transform
+
+
+def make_impact(
+    chosen_cats,
+    kcal_est,
+    val_fields,
+    pur_recs,
+    prod_codes,
+    prod_vals,
+    prod_meta,
+    nut_rec,
+):
+    """
+    Generate data needed to determine impact of reformulation on the population.
+
+    Parameters
+    ----------
+    chosen_cats : pd.DataFrame
+        pandas DataFrame with list of detailed categories nested within broader categories.
+    kcal_est : pd.DataFrame
+        pandas DataFrame with values for calorie reduction in case of reformulation
+    val_fields : pd.DataFrame
+        Pandas dataframe with codes to merge product master and uom dfs
+    pur_recs : pd.DataFrame
+        Pandas dataframe contains the purchase records of specified data
+    prod_codes : pd.DataFrame
+        Pandas dataframe contains the codes to link products to category information
+    prod_vals : pd.DataFrame
+        Pandas dataframe contains the product category information
+    prod_meta : pd.DataFrame
+        Pandas dataframe contains the product information
+    nut_rec : pd.DataFrame
+        Pandas dataframe contains the nutritional information of specified data.
+
+    Returns
+    -------
+    pandas dataframe with values of calories per household under different scenarios
+
+    """
+
+    target_red = chosen_cats.merge(kcal_est, on="rst_4_market_sector")
+
+    # Purchase and product info combined
+    comb_files = transform.combine_files(
+        val_fields, pur_recs, prod_codes, prod_vals, 2907
+    ).drop("att_vol", axis=1)
+
+    comb_update = comb_files.merge(
+        prod_meta[["product_code", "rst_4_extended", "rst_4_market_sector"]],
+        left_on="Product Code",
+        right_on="product_code",
+    )
+
+    comb_update.rename(columns={"rst_4_extended": "att_vol"}, inplace=True)
+
+    # get number of periods each hh is present
+    period_n = (
+        comb_update[["Panel Id", "Period"]]
+        .drop_duplicates()
+        .groupby(["Panel Id"])["Period"]
+        .size()
+        .reset_index(name="period_n")
+    )
+    period_n["days"] = period_n["period_n"] * 28
+
+    # sum of all kcal purchased by category by an household
+    purch_recs_comb = transform.make_purch_records(nut_rec, comb_update, ["att_vol"])
+
+    purch_recs_comb_scenarios = (
+        purch_recs_comb.merge(
+            target_red, right_on="rst_4_extended", left_on="att_vol", how="left"
+        )
+        .fillna(0)
+        .merge(period_n, on="Panel Id")
+    )
+
+    purch_recs_comb_scenarios["Gross_up_kcal_min"] = purch_recs_comb_scenarios[
+        "Gross_up_kcal"
+    ] * (1 - purch_recs_comb_scenarios["min"])
+    purch_recs_comb_scenarios["Gross_up_kcal_max"] = purch_recs_comb_scenarios[
+        "Gross_up_kcal"
+    ] * (1 - purch_recs_comb_scenarios["max"])
+
+    return purch_recs_comb_scenarios
+
+
+def kcal_day(purch_recs_comb_scenarios, pan_ind, panel_weight, scenario, low, high):
     """
     Returns summary statistics for a given scenario (no reformulation, high reformulation, low reformulation)
 
@@ -6,18 +93,24 @@ def kcal_day(purch_recs_comb, pan_conv, panel_weight, scenario):
     ----------
     purch_recs_comb : pd.DataFrame
         pandas DataFrame with sum of all kcal purchased by category by an household
-    pan_conv : pd.DataFrame
+    pan_ind : pd.DataFrame
         pandas DataFrame with conversion factor.
     panel_weight : pd.DataFrame
         pandas DataFrame with demographic weights.
     scenario : LIST
         ["Gross_up_kcal", "Gross_up_kcal_max", "Gross_up_kcal_min"]
+    low : float
+        percentile for trimming outliers at the bottom
+    high : float
+        percentile for trimming outliers at the top
 
     Returns
     -------
     pandas Series with descriptive statistics for daily kcal distribution
 
     """
+    # generate conversion factor
+    pan_conv = transform.hh_size_conv(pan_ind)
 
     pan_conv_weighted = pan_conv.merge(
         panel_weight, left_on="Panel Id", right_on="panel_id", how="inner"
@@ -28,13 +121,25 @@ def kcal_day(purch_recs_comb, pan_conv, panel_weight, scenario):
     pan_conv_weighted = pan_conv_weighted[["Panel Id", "conversion"]]
 
     hh_kcal = (
-        purch_recs_comb.groupby(["Panel Id"])[scenario].sum().reset_index(name=scenario)
+        purch_recs_comb_scenarios.groupby(["Panel Id", "days"])[scenario]
+        .sum()
+        .reset_index(name=scenario)
     )
 
     hh_kcal_weighted = hh_kcal.merge(pan_conv_weighted, on="Panel Id", how="inner")
 
     hh_kcal_weighted[scenario + "_daily"] = (
-        hh_kcal_weighted[scenario] / hh_kcal_weighted["conversion"] / 365
+        hh_kcal_weighted[scenario]
+        / hh_kcal_weighted["conversion"]
+        / hh_kcal_weighted["days"]
     )
 
-    return hh_kcal_weighted[scenario + "_daily"].describe()
+    q_hi = hh_kcal_weighted[scenario + "_daily"].quantile(high)
+    q_low = hh_kcal_weighted[scenario + "_daily"].quantile(low)
+
+    hh_kcal_filter = hh_kcal_weighted[
+        (hh_kcal_weighted[scenario + "_daily"] < q_hi)
+        & (hh_kcal_weighted[scenario + "_daily"] > q_low)
+    ].copy()
+
+    return hh_kcal_filter[scenario + "_daily"].describe()
