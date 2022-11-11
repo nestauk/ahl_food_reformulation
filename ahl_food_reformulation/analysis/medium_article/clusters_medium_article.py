13da6f1eb58bfbd89205cd8405ab65de6db3bb36
Isabel <isabel.stewart@nesta.org.uk>
2022-11-11 13:16:24 +0000

    code for medium article

---
+++ ahl_food_reformulation/analysis/medium_article/clusters_medium_article.py
@@ -0,0 +1,197 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     comment_magics: true
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.13.2
+#   kernelspec:
+#     display_name: ahl_food_reformulation
+#     language: python
+#     name: ahl_food_reformulation
+# ---
+
+# %% [markdown]
+# # Clustering households based on purchasing activity
+
+# %%
+# Import libraries and directory
+from ahl_food_reformulation import PROJECT_DIR
+from ahl_food_reformulation.getters import kantar
+from ahl_food_reformulation.pipeline import transform_data as transform
+from ahl_food_reformulation.pipeline import cluster_methods as cluster
+from ahl_food_reformulation.analysis.medium_article import (
+    functions_medium_article as medium,
+)
+import logging
+from pathlib import Path
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import seaborn as sns
+import numpy as np
+
+# %% [markdown]
+# ### Making household representations
+
+# %%
+# Get data
+logging.info("loading data")
+purch_recs_subset = kantar.purchase_subsets(202111)
+nut_subset = kantar.nutrition_subsets(202111)
+pan_ind = kantar.household_ind()
+prod_mast = kantar.product_master()
+val_fields = kantar.val_fields()
+uom = kantar.uom()
+prod_codes = kantar.product_codes()
+prod_vals = kantar.product_values()
+
+# %%
+logging.info("Create representations")
+# Scaler
+scaler = MinMaxScaler()
+# Converted household size
+pan_conv = transform.hh_size_conv(pan_ind)
+# Add volume measurement
+pur_vol = transform.vol_for_purch(purch_recs_subset, val_fields, prod_mast, uom)
+# Purchase and product info combined
+comb_files = transform.combine_files(
+    val_fields, pur_vol, prod_codes, prod_vals, 2829  # granular == 2907
+)
+# Household kcal per category adjusted for size - Make representation
+kcal_adj_subset = transform.hh_kcal_volume_converted(
+    nut_subset, pan_conv, scaler, comb_files
+)
+# Share of household kcal per category - Make representation
+kcal_share_subset = transform.hh_kcal_per_category(nut_subset, scaler, comb_files)
+
+# %%
+logging.info("Dimensionality reduction")
+# Using PCA and UMAP to reduce dimensions
+umap_adj_sub = cluster.dimension_reduction(kcal_adj_subset, 0.97)
+umap_share_sub = cluster.dimension_reduction(kcal_share_subset, 0.97)
+
+# %% [markdown]
+# ### Clustering households
+
+# %%
+logging.info("Apply kmeans to representations")
+
+# Get silhoutte scores for different numbers of k
+no_clusters = [10, 20, 30, 40, 50, 60, 70, 80]
+scores_share = cluster.kmeans_score_list(no_clusters, umap_share_sub)
+scores_adj = cluster.kmeans_score_list(no_clusters, umap_adj_sub)
+
+# %%
+# Plot results
+fig = plt.figure(figsize=(7, 5))
+sns.scatterplot(x=no_clusters, y=scores_share)
+sns.scatterplot(x=no_clusters, y=scores_adj)
+sns.lineplot(x=no_clusters, y=scores_share)
+sns.lineplot(x=no_clusters, y=scores_adj)
+
+fig.legend(
+    labels=["Kcal share", "Kcal adjusted for household size"],
+    bbox_to_anchor=(0.86, 0.35, 0.5, 0.5),
+    fontsize=11,
+)
+
+plt.scatter(
+    x=no_clusters[np.argmax(scores_share)], y=max(scores_share), color="b", alpha=1
+)
+plt.scatter(
+    x=no_clusters[np.argmax(scores_adj)], y=max(scores_adj), color="orange", alpha=1
+)
+
+plt.text(
+    (no_clusters[np.argmax(scores_share)] - 1),
+    (max(scores_share) - 0.012),
+    max(scores_share).round(3),
+)
+plt.text(
+    (no_clusters[np.argmax(scores_adj)] - 4),
+    (max(scores_adj) - 0.012),
+    max(scores_adj).round(3),
+)
+
+
+plt.xlabel("Number of clusters", fontsize=12)
+plt.ylabel("Silhoutte score", fontsize=12)
+plt.title(
+    "Silhoutte scores for adjusted and share of kcal representations",
+    fontsize=14,
+    pad=10,
+)
+plt.show()
+
+# %% [markdown]
+# ### Lets look at the clusters on a 2d map
+
+# %%
+### Testing different representations using kmeans
+logging.info("Plot kmeans representations")
+
+# %%
+# Picking best performing number of clusters from previous test
+share_clust_num = no_clusters[np.argmax(scores_share)]
+adj_clust_num = no_clusters[np.argmax(scores_adj)]
+
+# %%
+logging.info("Share of kcal")
+# Get kmeans labels
+_, kmeans_labels = cluster.kmeans_score(share_clust_num, umap_share_sub)
+# Plot clusters
+medium.plot_clusters("Share of kcal:", share_clust_num, umap_share_sub, kmeans_labels)
+
+# %%
+logging.info("Adjusted share of kcal")
+# Get kmeans labels
+_, kmeans_labels = cluster.kmeans_score(adj_clust_num, umap_adj_sub)
+# Plot clusters
+medium.plot_clusters(
+    "Adjusted share of kcal:", adj_clust_num, umap_adj_sub, kmeans_labels
+)
+
+# %% [markdown]
+# ### Deep dive into best performing clusters
+
+# %% [markdown]
+# #### Testing SHAP
+
+# %%
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import label_binarize
+from sklearn.cluster import KMeans
+import shap
+
+# %%
+kmeanModel = KMeans(n_clusters=no_clusters[np.argmax(scores_share)])
+y = kmeanModel.fit(umap_share_sub).labels_
+y = label_binarize(y, classes=list(range(no_clusters[np.argmax(scores_share)])))
+clf = RandomForestClassifier()
+clf.fit(kcal_share_subset, y)
+# clf.fit(kcal_share_subset.head(50), y[:50]) # testing smaller sample
+
+# %%
+shap.initjs()
+
+# %%
+explainer = shap.TreeExplainer(clf)
+
+# %%
+shap_values = explainer(kcal_share_subset).values
+
+# %%
+# X = kcal_share_subset
+
+# %%
+# visualize the first prediction’s explanation
+# shap.force_plot(explainer.expected_value[0], shap_values[0])
+
+# %%
+# shap.summary_plot(shap_values, X, plot_type='bar')
+
+# %%
---
+++ ahl_food_reformulation/analysis/medium_article/functions_medium_article.py
@@ -0,0 +1,81 @@
+# Import libraries and directory
+from array import array
+import numpy as np
+from matplotlib import pyplot as plt
+import matplotlib.cm as cm
+from sklearn.metrics import silhouette_samples, silhouette_score
+
+
+def plot_clusters(title: str, n_clusters: int, df: array, cluster_labels: list):
+    """
+    Plot of clustering based on number of k and labels
+    Args:
+        title (str): Description of representation to add to start of plot title
+        no_clusters (int): k
+        df (array): Umap representation of household representations
+        cluster_labels (list): List of assigned labels
+    """
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.set_size_inches(18, 7)
+    ax1.set_xlim([-0.6, 1])
+    ax1.set_ylim([0, len(df) + (n_clusters + 1) * 10])
+
+    silhouette_avg = silhouette_score(df, cluster_labels)
+    # Compute the silhouette scores for each sample
+    sample_silhouette_values = silhouette_samples(df, cluster_labels)
+    y_lower = 10
+
+    for i in range(n_clusters):
+        # Aggregate the silhouette scores for samples belonging to cluster i, and sort them
+        ith_cluster_silhouette_values = sample_silhouette_values[cluster_labels == i]
+        ith_cluster_silhouette_values.sort()
+        size_cluster_i = ith_cluster_silhouette_values.shape[0]
+        y_upper = y_lower + size_cluster_i
+        color = cm.nipy_spectral(float(i) / n_clusters)
+        ax1.fill_betweenx(
+            np.arange(y_lower, y_upper),
+            0,
+            ith_cluster_silhouette_values,
+            facecolor=color,
+            edgecolor=color,
+            alpha=0.7,
+        )
+        # Label the silhouette plots with their cluster numbers at the middle
+        ax1.text(-0.05, y_lower + 0.5 * size_cluster_i, str(i))
+
+        # Compute the new y_lower for next plot
+        y_lower = y_upper + 10  # 10 for the 0 samples
+
+    ax1.set_title("The silhouette plot for the various clusters.")
+    ax1.set_xlabel("The silhouette coefficient values")
+    ax1.set_ylabel("Cluster label")
+
+    # The vertical line for average silhouette score of all the values
+    ax1.axvline(x=silhouette_avg, color="red", linestyle="--")
+
+    ax1.set_yticks([])  # Clear the yaxis labels / ticks
+    ax1.set_xticks([-0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1])
+
+    # 2nd Plot showing the actual clusters formed
+    colors = cm.nipy_spectral(cluster_labels.astype(float) / n_clusters)
+    ax2.scatter(
+        df[:, 0],
+        df[:, 1],
+        marker=".",
+        s=30,
+        lw=0,
+        alpha=0.7,
+        c=colors,
+        edgecolor="k",
+    )
+
+    ax2.set_title("The visualization of the clustered data.")
+    ax2.set_xlabel("Feature space for the 1st feature")
+    ax2.set_ylabel("Feature space for the 2nd feature")
+
+    plt.suptitle(
+        title + " Silhouette analysis for clustering with n_clusters = %d" % n_clusters,
+        fontsize=14,
+        fontweight="bold",
+    )
+    plt.show()
