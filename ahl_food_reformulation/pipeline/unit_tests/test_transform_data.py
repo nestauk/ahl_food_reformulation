b3ab1c4f2be7e5b23554cb0a576f2bb092772b2e
Isabel <isabel.stewart@nesta.org.uk>
2022-12-06 19:00:06 +0000

    adding unit testing script for some functions in transform_data

---
+++ ahl_food_reformulation/pipeline/unit_tests/test_transform_data.py
@@ -0,0 +1,75 @@
+## Unit tests for transform_data functions in pipeline
+
+# Import libraries
+import pytest
+import pandas as pd
+from ahl_food_reformulation.pipeline import transform_data
+
+
+# Set fixture dfs to use in tests
+@pytest.fixture()
+def df():
+    df = pd.DataFrame(
+        [[50, 50], [400, 600]], index=["hh1", "hh2"], columns=["cat1", "cat2"]
+    )
+
+    return df
+
+
+@pytest.fixture()
+def purchases():
+    purchases = pd.DataFrame(
+        [
+            [5, 50, 100, "panel_1", 3, "xx1"],
+            [0, 100, 100, "panel_2", 3, "xx1"],
+            [10, 150, 100, "panel_1", 3, "xx1"],
+            [5, 200, 100, "panel_1", 3, "xx2"],
+            [1, 3, 100, "panel_3", 3, "xx4"],
+        ],
+        columns=[
+            "Volume",
+            "Energy KCal",
+            "Gross Up Weight",
+            "Panel Id",
+            "Quantity",
+            "Category_id",
+        ],
+    )
+    return purchases
+
+
+# proportion_hh
+def test_proportion_hh(df):
+    """Tests the proportion_hh function using dummy dataset"""
+    # Expected
+    df_exp = pd.DataFrame(
+        [[0.5, 0.5], [0.4, 0.6]], index=["hh1", "hh2"], columns=["cat1", "cat2"]
+    )
+    # test the result
+    assert transform_data.proportion_hh(df).equals(df_exp)
+
+
+# total_product_hh_purchase
+def test_total_product_hh_purchase(purchases):
+    """Tests the total_product_hh_purchase function using dummy dataset"""
+    # Expected
+    df_exp = pd.DataFrame(
+        [
+            ["panel_1", "xx1", 15, 200, 6, 200, 20000],
+            ["panel_1", "xx2", 5, 200, 3, 100, 20000],
+            ["panel_3", "xx4", 1, 3, 3, 100, 300],
+        ],
+        columns=[
+            "Panel Id",
+            "Category_id",
+            "Volume",
+            "Energy KCal",
+            "Quantity",
+            "Gross Up Weight",
+            "Gross_up_kcal",
+        ],
+    )
+    # test the result
+    assert transform_data.total_product_hh_purchase(purchases, ["Category_id"]).equals(
+        df_exp
+    )
